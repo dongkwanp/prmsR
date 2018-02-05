6d67f8482586c098bfdaaf38dabf1c4517d7e896
Don Park <donpark@snu.ac.kr>
2018-02-05 12:34:06 -0500

    Note regarding units (probably cfs)

--- R/OutputRead.R
+++ R/OutputRead.R
@@ -1,17 +1,17 @@
 #' Parse and Read-In Statvar Output file
-#' 
-#' @param output.filepath Output file path to read in
-#' 
+#'
+#' @param output.filepath Output file path to read in (unit is probably cfs)
+#'
 #' @return output.output
-#' @export 
+#' @export
 
 OutputRead <- function(output.filepath = paste0(getwd(), '/output/output.statvar')) {
-  
+
   rawimport <- read.csv(output.filepath, header = FALSE, sep = " ", skip = 3, col.names = c("TimeStep", "Year", "Month", "Day", "Hour", "Min", "Sec", "Flow", "BasinPWEquiv", "NA"))
-  
+
   Date <- as.Date(paste(rawimport$Year, rawimport$Month, rawimport$Day, sep = "-"), "%Y-%m-%d")
-  
+
   output.output <- data.frame(Date, rawimport$Flow)
-  
+
   return(output.output)
 }
