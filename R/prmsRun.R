862270aef6c1b553bd95bf488ff5355aad0766cd
Don Park <donpark@snu.ac.kr>
2018-02-05 13:00:39 -0500

    PRMS Run function commit

---
+++ R/prmsRun.R
@@ -0,0 +1,15 @@
+#' PRMS Run Wrapper
+#'
+#' @param binary.filepath Filepath to the PRMS Binary (Relative or Absolute)
+#' @param control.filepath Filepath to the control file (Relative or Absolute)
+#'
+#' @return NULL
+#'
+#' @export
+
+
+prmsRun <- function(binary.filepath, control.filepath) {
+
+  system(paste(binary.filepath, control.filepath, sep = " "))
+
+}
