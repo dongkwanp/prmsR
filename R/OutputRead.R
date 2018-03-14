c05d6038aeebb7d2bb941e8bd36e240b1e327624
Don Park <donpark@umass.edu>
2018-03-14 16:21:05 -0400

    Modified 'OutputRead' function to return all the outputs in the STATVAR file.

--- R/OutputRead.R
+++ R/OutputRead.R
@@ -7,11 +7,29 @@
 
 OutputRead <- function(output.filepath = paste0(getwd(), '/output/output.statvar')) {
 
-  rawimport <- read.csv(output.filepath, header = FALSE, sep = " ", skip = 3, col.names = c("TimeStep", "Year", "Month", "Day", "Hour", "Min", "Sec", "Flow", "BasinPWEquiv", "NA"))
+  # Determining Header Names
+  conOutputFile <- file(output.filepath, open = 'r')
+
+  skiplength <- as.numeric(readLines(conOutputFile, n = 1))
+
+  listnames <- rep(NA, skiplength)
+
+  for (i in 1:skiplength) {
+
+    listnames[i] <- gsub("([A-Za-z]+_[A-Za-z]+).*", "\\1", readLines(conOutputFile, n = 1))
+
+  }
+
+  close(conOutputFile)
+
+  # Generating Header Names
+  column.names <- append(c('Timestep', 'Year', 'Month', 'Day', 'Hour', 'Min', 'Sec', 'NA'), listnames, after = 7)
+
+  rawimport <- read.csv(output.filepath, header = FALSE, sep = " ", skip = (skiplength + 1), col.names = column.names)
 
   Date <- as.Date(paste(rawimport$Year, rawimport$Month, rawimport$Day, sep = "-"), "%Y-%m-%d")
 
-  output.output <- data.frame(Date, rawimport$Flow)
+  output.output <- data.frame(Date, rawimport[ ,8:(length(column.names) - 1)])
 
   return(output.output)
 }
